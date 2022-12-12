46542fd0f5488d58c8178bdb92c4a949c7a4f03f
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-12-12 23:13:10 +0100

    Modified components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,39 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HAMMER_GESTURE_CONFIG, HammerModule } from '@angular/platform-browser';
 import { NgImageSliderModule } from 'ng-image-slider';
-import {MatCardModule} from '@angular/material/card';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SlideResolutionComponent } from './slide-resolution/slide-resolution.component';
-import { ProductsComponent } from './products/products.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LyHammerGestureConfig, LY_THEME, LY_THEME_NAME, StyleRenderer, LyTheme2 } from '@alyle/ui';
+import { MinimaLight } from '@alyle/ui/themes/minima';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LyImageCropperModule } from '@alyle/ui/image-cropper';
+import { LySliderModule } from '@alyle/ui/slider';
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyIconModule } from '@alyle/ui/icon';
+
 import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
+import { ProductsComponent } from './products/products.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { HomeComponent } from './home/home.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PasswordChangeComponent } from './password-change/password-change.component';
+import { LoginComponent } from './login/login.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -45,12 +57,19 @@ import { PasswordChangeComponent } from './password-change/password-change.compo
     MatIconModule,
     MatButtonModule,
     MatCardModule,
+    ReactiveFormsModule,
     NgImageSliderModule,
     MatButtonToggleModule,
     BrowserAnimationsModule,
-    NgbModule
+    NgbModule,
+    HammerModule,
+    FormsModule,
+    LyImageCropperModule,
+    LySliderModule,
+    LyButtonModule,
+    LyIconModule
   ],
-  providers: [],
+  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: LyHammerGestureConfig }, StyleRenderer, LyTheme2, { provide: LY_THEME_NAME, useValue: 'minima-light' }, { provide: LY_THEME, useClass: MinimaLight, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
