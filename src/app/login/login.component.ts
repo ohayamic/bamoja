340b15449640e5fad9133983bfb226965db744ec
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-12-12 23:10:43 +0100

    Made form reactive

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,16 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+    loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    });
+
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value)
+  }
 }
