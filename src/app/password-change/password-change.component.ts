ab7b1f254ac0c1d75a35802d4b71cf1482d9dbf1
Ohaya Onyebuchi Michael <onyebuchi_ohaya@yahoo.co.uk>
2022-12-21 23:07:19 +0100

    Retrieved data from form

--- src/app/password-change/password-change.component.ts
+++ src/app/password-change/password-change.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-password-change',
@@ -6,10 +16,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-change.component.css']
 })
 export class PasswordChangeComponent implements OnInit {
+  password_matcher = new MyErrorStateMatcher();
+
+  passwordForm = new FormGroup({
+    oldpassword: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z@1-9#]*')]),
+    newpassword: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z@1-9#]*')])
+  })
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+    console.log(this.passwordForm.value)
   }
 
 }
